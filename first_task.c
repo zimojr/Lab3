6d34141da8d964a27b0b4c2128db4a64c5e7c184
nemmarton <103062588+nemmarton@users.noreply.github.com>
2025-08-28 11:29:22 +0200

    Create first_task.c

---
+++ first_task.c
@@ -0,0 +1,21 @@
+ /*
+ 1.Product of numbers
+
+Create a program that calculates the product of the first 10 numbers (1…10), which is 10 factorial. You may use the given pseudo code. 
+Put an extra line in the loop to print the value of the loop variable and that of the product as well in order to see how the product develops
+
+Let the product be 1.
+Let n be 10.
+As long as n≥2
+    Let product be product × n.
+    Decrease n by 1.
+End of repetition
+Print the pruduct.*/
+
+#include<stdio.h>
+
+int main(){
+
+
+
+}
