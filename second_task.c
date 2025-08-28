08e733b5d63c98b1e0cecde41c44d1ec20e45d69
nemmarton <103062588+nemmarton@users.noreply.github.com>
2025-08-28 11:32:33 +0200

    Create second_task.c

---
+++ second_task.c
@@ -0,0 +1,14 @@
+/*Calculate the sum of divisors for a number entered by the user. (e.g. for 6: 1+2+3+6 = 12.) Modify your algorithm to exclude the number itself from the summation. What should be changed in the program?
+
+Perfect number is an integer that equals to this sum (so the sum of divisors, including 1, excluding the number itself). The first perfect number is 6 as 1+2+3=6. 
+The next two are 28 and 496. Print out if the number entered by the user is a perfect number or not.
+
+Hint: Sum of divisors implies applying summation: a loop, and summing in the accumulator. In our case not all numbers in the loop qualify for being a divisor. */
+
+#include <stdio.h>
+
+int main(){
+
+
+
+}
